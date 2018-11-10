26e766d7388e7488c6f3128dece71b2d684877e3
andreww7985 <andreww7985@gmail.com>
2018-11-10 10:56:23 +0200

    Add copy logs option to settings

--- app/src/main/java/me/andreww7985/connectplus/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/me/andreww7985/connectplus/ui/fragments/SettingsFragment.kt
@@ -1,19 +1,36 @@
 package me.andreww7985.connectplus.ui.fragments
 
+import android.content.ClipData
+import android.content.ClipboardManager
 import android.os.Bundle
+import androidx.core.content.getSystemService
 import androidx.preference.PreferenceFragmentCompat
 import me.andreww7985.connectplus.R
+import me.andreww7985.connectplus.helpers.UIHelper
 import me.andreww7985.connectplus.ui.FragmentName
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
 
 class SettingsFragment : PreferenceFragmentCompat(), FragmentName {
-    companion object {
-        const val TAG = "SettingsFragment"
-    }
-
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         addPreferencesFromResource(R.xml.preferences)
-    }
 
+        findPreference("copy_logs").setOnPreferenceClickListener {
+            val process = Runtime.getRuntime().exec("logcat -d")
+            val bufferedReader = BufferedReader(InputStreamReader(process.inputStream))
+            val log = StringBuilder()
+            while (true) {
+                log.appendln(bufferedReader.readLine() ?: break)
+            }
+
+            val clipboardManager = context!!.getSystemService<ClipboardManager>()!!
+            clipboardManager.primaryClip = ClipData.newPlainText("logs", log)
+
+            UIHelper.showToast("Copied logs to clipboard")
+            false
+        }
+    }
 
     override fun getName() = "Settings"
 }
