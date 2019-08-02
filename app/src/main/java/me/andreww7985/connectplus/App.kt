21ab80cfcdafea6bbdaa75b90db466de1ead295d
andreww7985 <andreww7985@gmail.com>
2019-08-02 13:24:12 +0300

    Fix crashlytics reporting

--- app/src/main/java/me/andreww7985/connectplus/App.kt
+++ app/src/main/java/me/andreww7985/connectplus/App.kt
@@ -3,6 +3,8 @@ package me.andreww7985.connectplus
 import android.app.Application
 import android.content.SharedPreferences
 import androidx.preference.PreferenceManager
+import com.crashlytics.android.Crashlytics
+import io.fabric.sdk.android.Fabric
 import timber.log.Timber
 
 class App : Application() {
@@ -20,7 +22,16 @@ class App : Application() {
 
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
 
+        val sendUsageData = sharedPreferences.getBoolean("send_usage_data", !BuildConfig.DEBUG)
+
+        Timber.d("sendUsageData $sendUsageData")
+
         analytics = Analytics()
-        analytics.setAnalyticsEnabled(sharedPreferences.getBoolean("send_usage_data", true))
+        analytics.setAnalyticsEnabled(sendUsageData)
+
+        if (sendUsageData) {
+            Fabric.with(this, Crashlytics())
+            Timber.d("Crashlytics enabled")
+        }
     }
 }
