c7c651d12a5fa46b077bce3e1629a49979f54e9e
andreww7985 <andreww7985@gmail.com>
2019-10-05 20:04:59 +0300

    Add support for dark theme

--- app/src/main/java/me/andreww7985/connectplus/App.kt
+++ app/src/main/java/me/andreww7985/connectplus/App.kt
@@ -2,6 +2,7 @@ package me.andreww7985.connectplus
 
 import android.app.Application
 import android.content.SharedPreferences
+import androidx.appcompat.app.AppCompatDelegate
 import androidx.preference.PreferenceManager
 import com.crashlytics.android.Crashlytics
 import io.fabric.sdk.android.Fabric
@@ -22,7 +23,7 @@ class App : Application() {
 
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this)
 
-        val sendUsageData = sharedPreferences.getBoolean("send_usage_data", !BuildConfig.DEBUG)
+        val sendUsageData = sharedPreferences.getBoolean("send_usage_data", true)
 
         Timber.d("sendUsageData $sendUsageData")
 
@@ -33,5 +34,18 @@ class App : Application() {
             Fabric.with(this, Crashlytics())
             Timber.d("Crashlytics enabled")
         }
+
+        updateDarkTheme()
+    }
+
+    fun updateDarkTheme() {
+        AppCompatDelegate.setDefaultNightMode(
+                when (sharedPreferences.getString("dark_theme", "SYSTEM")) {
+                    "ENABLED" -> AppCompatDelegate.MODE_NIGHT_YES
+                    "DISABLED" -> AppCompatDelegate.MODE_NIGHT_NO
+                    "SYSTEM" -> AppCompatDelegate.MODE_NIGHT_FOLLOW_SYSTEM
+                    else -> AppCompatDelegate.MODE_NIGHT_UNSPECIFIED
+                }
+        )
     }
 }
--- app/src/main/java/me/andreww7985/connectplus/ui/fragments/SettingsFragment.kt
+++ app/src/main/java/me/andreww7985/connectplus/ui/fragments/SettingsFragment.kt
@@ -2,17 +2,19 @@ package me.andreww7985.connectplus.ui.fragments
 
 import android.content.ClipData
 import android.content.ClipboardManager
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.core.content.getSystemService
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
+import me.andreww7985.connectplus.App
 import me.andreww7985.connectplus.R
 import me.andreww7985.connectplus.helpers.UIHelper
 import java.io.BufferedReader
 import java.io.InputStreamReader
 
 
-class SettingsFragment : PreferenceFragmentCompat() {
+class SettingsFragment : PreferenceFragmentCompat(), SharedPreferences.OnSharedPreferenceChangeListener {
     override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
         addPreferencesFromResource(R.xml.preferences)
 
@@ -28,6 +30,14 @@ class SettingsFragment : PreferenceFragmentCompat() {
 
             UIHelper.showToast("Copied logs to clipboard")
             false
+        }
+
+        App.sharedPreferences.registerOnSharedPreferenceChangeListener(this)
+    }
+
+    override fun onSharedPreferenceChanged(p0: SharedPreferences, key: String) {
+        if (key == "dark_theme") {
+            App.instance.updateDarkTheme()
         }
     }
 }
