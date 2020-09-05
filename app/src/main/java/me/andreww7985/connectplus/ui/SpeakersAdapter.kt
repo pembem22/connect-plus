a3c85978340f719b05093f25e40474480c4528db
pembem22 <24252798+pembem22@users.noreply.github.com>
2020-09-05 14:10:49 +0300

    Use material button group for channel selector

--- app/src/main/java/me/andreww7985/connectplus/ui/SpeakersAdapter.kt
+++ app/src/main/java/me/andreww7985/connectplus/ui/SpeakersAdapter.kt
@@ -1,6 +1,5 @@
 package me.andreww7985.connectplus.ui
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,30 +9,40 @@ import me.andreww7985.connectplus.R
 import me.andreww7985.connectplus.protocol.AudioChannel
 import me.andreww7985.connectplus.speaker.Feature
 import me.andreww7985.connectplus.speaker.SpeakerModel
+import java.util.*
 
-class SpeakersAdapter(val context: Context, val speakers: List<SpeakerModel>) : RecyclerView.Adapter<ViewHolder>() {
+class SpeakersAdapter(private val speakers: List<SpeakerModel>) : RecyclerView.Adapter<ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        return ViewHolder(LayoutInflater.from(context).inflate(R.layout.item_speaker, parent, false))
+        return ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_speaker, parent, false))
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
         val speaker = speakers[position]
+        val context = holder.itemView.context
 
-        val speakerDrawableId = context.resources.getIdentifier(String.format("img_%s_%s", speaker.model.name.toLowerCase(), speaker.color.name.toLowerCase()), "drawable", context.packageName)
+        val speakerDrawableId = context.resources.getIdentifier(String.format("img_%s_%s", speaker.model.name.toLowerCase(Locale.ROOT), speaker.color.name.toLowerCase(Locale.ROOT)), "drawable", context.packageName)
         if (speakerDrawableId != 0) holder.speakerImage.setImageResource(speakerDrawableId)
 
         holder.soundButton.setOnClickListener { speaker.playSound() }
 
-        holder.nameLabel.text = speaker.getFeature<Feature.BatteryName>()!!.deviceName
+        holder.nameLabel.text = speaker.getFeature<Feature.BatteryName>().deviceName
 
-        holder.leftButton.isEnabled = speaker.audioChannel != AudioChannel.LEFT
-        holder.leftButton.setOnClickListener { speaker.updateAudioChannel(AudioChannel.LEFT) }
+        holder.channelButtons.check(when (speaker.audioChannel) {
+            AudioChannel.LEFT -> R.id.speaker_left_button
+            AudioChannel.RIGHT -> R.id.speaker_right_button
+            AudioChannel.STEREO -> R.id.speaker_stereo_button
+        })
 
-        holder.stereoButton.isEnabled = speaker.audioChannel != AudioChannel.STEREO
-        holder.stereoButton.setOnClickListener { speaker.updateAudioChannel(AudioChannel.STEREO) }
-
-        holder.rightButton.isEnabled = speaker.audioChannel != AudioChannel.RIGHT
-        holder.rightButton.setOnClickListener { speaker.updateAudioChannel(AudioChannel.RIGHT) }
+        holder.channelButtons.addOnButtonCheckedListener { _, checkedId, isChecked ->
+            if (isChecked) {
+                speaker.updateAudioChannel(when (checkedId) {
+                    R.id.speaker_left_button -> AudioChannel.LEFT
+                    R.id.speaker_right_button -> AudioChannel.RIGHT
+                    R.id.speaker_stereo_button -> AudioChannel.STEREO
+                    else -> throw IllegalArgumentException("Illegal button ID on channel selector")
+                })
+            }
+        }
     }
 
     override fun getItemCount() = speakers.size
@@ -42,8 +51,6 @@ class SpeakersAdapter(val context: Context, val speakers: List<SpeakerModel>) : 
 class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
     val speakerImage = view.speaker_image!!
     val nameLabel = view.speaker_name_value!!
-    val leftButton = view.speaker_left_button!!
-    val stereoButton = view.speaker_stereo_button!!
-    val rightButton = view.speaker_right_button!!
     val soundButton = view.speaker_sound_button!!
+    val channelButtons = view.speaker_channel_buttons!!
 }
