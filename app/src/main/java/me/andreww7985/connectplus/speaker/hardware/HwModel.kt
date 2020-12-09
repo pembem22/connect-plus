8400f64c13368237b0b25a2321d1a9a6f207b99f
pembem22 <24252798+pembem22@users.noreply.github.com>
2020-12-09 19:43:48 +0200

    Refactor speaker model recognition

--- app/src/main/java/me/andreww7985/connectplus/speaker/hardware/HwModel.kt
+++ app/src/main/java/me/andreww7985/connectplus/speaker/hardware/HwModel.kt
@@ -34,6 +34,20 @@ enum class HwModel(val value: Int, val modelName: String) {
     }
 
     companion object {
-        fun from(value: Int) = values().firstOrNull { it.value == value } ?: UNKNOWN
+        fun from(modelId: Int) = when (modelId) {
+            0x0023 /* CSR */ -> FLIP3
+            0x0024 /* CSR */ -> XTREME
+            0x0026 /* CSR */ -> PULSE2
+            0x1EBC /* CSR */, 0x1F25 /* QCC */ -> CHARGE3
+            0x1ED1 /* CSR */, 0x1F24 /* QCC */ -> FLIP4
+            0x1ED2 /* CSR */, 0x1F28 /* QCC */ -> PULSE3
+            0x1EE7 /* CSR */, 0x1F27 /* QCC */ -> BOOMBOX
+            0x1EFC /* CSR */, 0x1F26 /* QCC */ -> XTREME2
+            0x1F17 /* CSR */, 0x1F29 /* QCC */ -> CHARGE4
+            0x1F31 /* VIMICRO */ -> FLIP5
+            0x1F53 /* VIMICRO */ -> BOOMBOX2
+            0x1F56 /* VIMICRO */ -> PULSE4
+            else -> UNKNOWN
+        }
     }
 }
